01a4c48342dd6289725ecc09dfcba74791c1667a
peter <peterhyun1234@gmail.com>
2019-11-19 02:33:17 +0900

    add function for parsing datas by using REGEX

--- Java_code/DB_TSET/src/DBTEST.java
+++ Java_code/DB_TSET/src/DBTEST.java
@@ -5,6 +5,8 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.io.DataOutputStream;
 import java.net.URL;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 // 자동정렬: ctrl + shift + f
 public class DBTEST {
@@ -46,7 +48,7 @@ public class DBTEST {
 		System.out.println("4. 사용자 요청 확인");
 		System.out.println("================================\n");
 		System.out.println("원하는 기능의 번호를 입력해주세요:");
-		
+
 		Input_str = scan.nextLine();
 
 		while (!Input_str.equals("exit")) {
@@ -72,11 +74,12 @@ public class DBTEST {
 		System.out.println("program을 종료합니다.");
 		return;
 	}
+
 	private static void show_user_list() {
 		// TODO Auto-generated method stub
 		System.out.println("<1. 회원 목록 확인>");
-		
-		GET("http://49.236.136.213:3000/user/show_all_users");		
+
+		GET("http://49.236.136.213:3000/user/show_all_users", "user");
 	}
 
 	private static void show_user_interest() {
@@ -90,8 +93,8 @@ public class DBTEST {
 		String uID = scan.nextLine();
 		String p_code = scan.nextLine();
 		String contents = scan.nextLine();
-		
-		String urlParameters = "review_uID =" + uID + "&p_code=" + p_code + "&contents=" + contents; 
+
+		String urlParameters = "review_uID =" + uID + "&p_code=" + p_code + "&contents=" + contents;
 		POST("http://49.236.136.213:3000/review/write_review", urlParameters);
 	}
 
@@ -101,13 +104,6 @@ public class DBTEST {
 
 	}
 
-
-	
-	
-	
-	
-	
-	
 	// 사용자용 프로그램
 	public static void user_program() {
 		System.out.println("================================");
@@ -168,7 +164,7 @@ public class DBTEST {
 		System.out.println("<3. 거주지 기반 정책 검색>");
 
 	}
-	
+
 	private static void category_based_search() {
 		// TODO Auto-generated method stub
 		System.out.println("<4. 지원분야 기반 검색>");
@@ -186,6 +182,7 @@ public class DBTEST {
 		System.out.println("<6. 건의사항 작성>");
 
 	}
+
 	// 로그인 실패
 	public static void signin_failure() {
 		while (!ID.equals("exit")) {
@@ -198,103 +195,111 @@ public class DBTEST {
 			{
 				admin_program();
 				break;
-			} 
-			else if (ID.equals("user") & pswd.equals("user")) // 사용자 로그인
+			} else if (ID.equals("user") & pswd.equals("user")) // 사용자 로그인
 			{
 				user_program();
 				break;
-			}			
+			}
 		}
 		System.out.println("program을 종료합니다.");
 		return;
 	}
-	
-	
-	// https://developers.naver.com/docs/common/openapiguide/apicall.md <-- 자바 REST API 관련!! 
-	// https://118k.tistory.com/225 <-- 자바 REST API 관련!! 
-	
-	//REST API - GET
-	public static void GET(String requestURL) {
-		
-        try {
 
-            URL url = new URL(requestURL);
-            HttpURLConnection con = (HttpURLConnection)url.openConnection();
-            con.setRequestMethod("GET");
-            //con.setRequestProperty("Authorization", header);
-            int responseCode = con.getResponseCode();
-            BufferedReader br;
-            if(responseCode==200) { // 정상 호출
-                br = new BufferedReader(new InputStreamReader(con.getInputStream(), "UTF-8"));
-            } else {  // 에러 발생
-                br = new BufferedReader(new InputStreamReader(con.getErrorStream(), "UTF-8"));
-            }
-            String inputLine;
-            //StringBuffer response = new StringBuffer();
+	// https://developers.naver.com/docs/common/openapiguide/apicall.md <-- 자바 REST
+	// API 관련!!
+	// https://118k.tistory.com/225 <-- 자바 REST API 관련!!
+	// https://highcode.tistory.com/6 <-- 자바 regex 관련
+	// https://itpangpang.xyz/280 <-- 자바 regex 관련
+	// http://egloos.zum.com/js7309/v/11117112 <-- 자바 regex 관련 (replaceall 정규식 사용)
 
-            while ((inputLine = br.readLine()) != null) {
-            	//System.out.println(inputLine);
-            	StringTokenizer str = new StringTokenizer(inputLine, ","); //StringTokenizer 클래스 생성, 파싱하려는 문자열, 구분자 "," 
- 
-            	
-            	while (str.hasMoreTokens()) {
-            		// https://lifeones.tistory.com/89 <-- 자바 파싱 관련!! 
-            		
-            		System.out.println(str.nextToken());
-            	}
+	// REST API - GET
+	public static void GET(String requestURL, String table) {
+		String user[] = { "uID", "name", "region", "age", "Employment_sup_priority", "Startup_sup_priority",
+				"Life_welfare_priority", "Residential_financial_priority" };
+		String user_change[] = { "\n사용자 ID ", "사용자 이름 ", "사용자 사는 지역 ", "사용자 나이 ", "취업 관련 정책 선호도", "창업 관련 정책 선호도",
+				"복지*생활 정책 선호도", "주거*금융 정책 선호도" };
 
-            }
-            br.close();
-            //System.out.println(response.toString());
-        } catch (Exception e) {
-            System.out.println(e);
-        }
+		try {
+
+			URL url = new URL(requestURL);
+			HttpURLConnection con = (HttpURLConnection) url.openConnection();
+			con.setRequestMethod("GET");
+			// con.setRequestProperty("Authorization", header);
+			int responseCode = con.getResponseCode();
+			BufferedReader br;
+			if (responseCode == 200) { // 정상 호출
+				br = new BufferedReader(new InputStreamReader(con.getInputStream(), "UTF-8"));
+			} else { // 에러 발생
+				br = new BufferedReader(new InputStreamReader(con.getErrorStream(), "UTF-8"));
+			}
+			String inputLine;
+
+			while ((inputLine = br.readLine()) != null) {
+				String match = "[^\uAC00-\uD7A3xfe0-9a-zA-Z\\s.: @_,]";
+				String convert = inputLine.replaceAll(match, "");
+
+				if (table == "user") {
+					for (int i = 0; i < user.length; i++) {
+						convert = convert.replaceAll(user[i], user_change[i]);
+					}
+				}
+				// System.out.println(convert);
+
+				String convertArray[] = convert.split(",");
+				int convert_arr_len = convertArray.length;
+				int cnt = 0;
+
+				while (cnt < convert_arr_len) {
+					System.out.println(convertArray[cnt]);
+					cnt++;
+				}
+
+			}
+			br.close();
+			// System.out.println(response.toString());
+		} catch (Exception e) {
+			System.out.println(e);
+		}
 	}
-	
-	//REST API - POST
+
+	// REST API - POST
 	public static void POST(String requestURL, String postParameters) {
-		
 
-        try {
+		try {
 
+			URL url = new URL(requestURL);
+			HttpURLConnection con = (HttpURLConnection) url.openConnection();
+			con.setRequestMethod("POST");
 
-            URL url = new URL(requestURL);
-            HttpURLConnection con = (HttpURLConnection)url.openConnection();
-            con.setRequestMethod("POST");
+			// post request
+			con.setDoOutput(true);
+			DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+			wr.writeUTF(postParameters);
+			wr.flush();
+			wr.close();
 
-            // post request
-            con.setDoOutput(true);
-            DataOutputStream wr = new DataOutputStream(con.getOutputStream());
-            wr.writeUTF(postParameters);
-            wr.flush();
-            wr.close();
-            
-            int responseCode = con.getResponseCode();
-            BufferedReader br;
-            if(responseCode==200) { // 정상 호출
-                br = new BufferedReader(new InputStreamReader(con.getInputStream(), "UTF-8"));
-            } else {  // 에러 발생
-                br = new BufferedReader(new InputStreamReader(con.getErrorStream(), "UTF-8"));
-            }
-            String inputLine;
+			int responseCode = con.getResponseCode();
+			BufferedReader br;
+			if (responseCode == 200) { // 정상 호출
+				br = new BufferedReader(new InputStreamReader(con.getInputStream(), "UTF-8"));
+			} else { // 에러 발생
+				br = new BufferedReader(new InputStreamReader(con.getErrorStream(), "UTF-8"));
+			}
+			String inputLine;
 
+			while ((inputLine = br.readLine()) != null) {
 
-            while ((inputLine = br.readLine()) != null) {
+				StringTokenizer str = new StringTokenizer(inputLine, ",");
 
-            	StringTokenizer str = new StringTokenizer(inputLine, ",");
+				while (str.hasMoreTokens()) {
+					System.out.println(str.nextToken());
+				}
 
-            	while (str.hasMoreTokens()) {
-            		System.out.println(str.nextToken());
-            	}
-
-            }
-            br.close();
-        } catch (Exception e) {
-            System.out.println(e);
-        }
+			}
+			br.close();
+		} catch (Exception e) {
+			System.out.println(e);
+		}
 	}
-	
+
 }
-
-
-
